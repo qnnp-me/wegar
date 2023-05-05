54355bdc5bfac4bd41fc21a9a465c8d7812320ff
qnnp-me <wujipeng@foxmail.com>
2023-05-05 14:50:22 +0800

    优化文档缓存 header

--- src/Module/WegarAuthMiddleware.php
+++ src/Module/WegarAuthMiddleware.php
@@ -18,22 +18,24 @@ class WegarAuthMiddleware implements MiddlewareInterface
 	 */
 	public function process(Request $request, callable $handler): Response
 	{
+		$headers = [
+			'Cache-Control' => 'no-cache'
+		];
 		if (class_exists(AccessControl::class)) {
-			return (new AccessControl())->process($request, $handler);
-		} elseif ($conf_passwd=Wegar::config('password','')) {
+			return (new AccessControl())->process($request, $handler)->withHeaders($headers);
+		} elseif ($conf_passwd = Wegar::config('password', '')) {
 			$passwd = $request->post('wegar-auth');
-			if ($passwd===$conf_passwd) {
+			if ($passwd === $conf_passwd) {
 				$request->session()->set('wegar-auth', md5($passwd));
-				return response("<script>navigation.back()</script>"
-				);
+				return response("<script>navigation.back()</script>")->withHeaders($headers);
 			}
 			if (session('wegar-auth') === md5($conf_passwd)) {
-				return $handler($request);
+				return $handler($request)->withHeaders($headers);
 			} else {
-				return response('')->file(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'public/swagger/auth.html');
+				return response('')->file(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'public/swagger/auth.html')->withHeaders($headers);
 			}
 		} else {
-			return $handler($request);
+			return $handler($request)->withHeaders($headers);
 		}
 	}
 }
