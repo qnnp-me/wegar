da2dafc7ab1845ed551c621490c2e3639dd93e29
qnnp-me <wujipeng@foxmail.com>
2023-05-09 19:39:53 +0800

    @???

--- src/Attribute/BasePath.php
+++ src/Attribute/BasePath.php
@@ -4,10 +4,13 @@ namespace qnnp\wegar\Attribute;
 
 use Attribute;
 
+/**
+ * 为控制器下所有相对路径路由指定基础路径
+ */
 #[Attribute(Attribute::TARGET_CLASS)]
 class BasePath
 {
-    public function __construct(public string $path = '')
-    {
-    }
+  public function __construct(public string $path = '')
+  {
+  }
 }
--- src/Module/Wegar.php
+++ src/Module/Wegar.php
@@ -16,6 +16,7 @@ namespace qnnp\wegar\Module;
 use plugin\admin\api\Menu;
 use qnnp\wegar\Attribute\BasePath;
 use qnnp\wegar\Attribute\Middleware;
+use qnnp\wegar\Attribute\Prefix;
 use qnnp\wegar\Attribute\RemoveFromDoc;
 use qnnp\wegar\Attribute\Route as RouteAttribute;
 use qnnp\wegar\Controller\WegarController;
@@ -27,200 +28,210 @@ use Webman\MiddlewareInterface;
 class Wegar
 {
 
-	/**
-	 * <h2 style="color:#E97230;">扫描注解路由</h2>
-	 * <span style="color:#E97230;">/app 默认自动加载</span>
-	 *
-	 * @param array $apps <span style="color:#E97230;">需要另外加载的 app , 此处 App::class 为 app Controller 目录的一个类</span>
-	 *                    <pre style="color:#E97230;">[ App::class, ...]</pre>
-	 *
-	 * @param bool $init <span style="color:#E97230;">加载默认路由</span>
-	 */
-	static function scan(array $apps = [], bool $init = false): void
-	{
-		if ($init) {
-			$controller_class_list = static::scanControllerClasses('\app', app_path());
-			foreach ($controller_class_list as $controller_class => $app_base_namespace) {
-				static::scanController($controller_class, $app_base_namespace);
-			}
-			static::scanAppController(WegarController::class);
-			self::checkMenu();
-		}
+  /**
+   * <h2 style="color:#E97230;">扫描注解路由</h2>
+   * <span style="color:#E97230;">/app 默认自动加载</span>
+   *
+   * @param array $apps <span style="color:#E97230;">需要另外加载的 app , 此处 App::class 为 app Controller 目录的一个类</span>
+   *                    <pre style="color:#E97230;">[ App::class, ...]</pre>
+   *
+   * @param bool $init <span style="color:#E97230;">加载默认路由</span>
+   */
+  static function scan(array $apps = [], bool $init = false): void
+  {
+    if ($init) {
+      $controller_class_list = static::scanControllerClasses('\app', app_path());
+      foreach ($controller_class_list as $controller_class => $app_base_namespace) {
+        static::scanController($controller_class, $app_base_namespace);
+      }
+      static::scanAppController(WegarController::class);
+      self::checkMenu();
+    }
 
-		foreach ($apps as $app) {
-			if (class_exists($app)) {
-				static::scanAppController($app);
-			}
-		}
-	}
+    foreach ($apps as $app) {
+      if (class_exists($app)) {
+        static::scanAppController($app);
+      }
+    }
+  }
 
-	private static function scanControllerClasses(string $app_base_namespace, string $app_base_path): array
-	{
-		$controller_class_list = [];
-		$app_base_path = realpath($app_base_path) ?: $app_base_path;
-		$controller_files = static::scanControllerFiles($app_base_path);
-		foreach ($controller_files as $controller_file) {
-			$controller_class = preg_replace("/\.php$/i", '', $controller_file);
-			$controller_class = str_replace($app_base_path, $app_base_namespace, $controller_class);
-			$controller_class = str_replace("/", '\\', $controller_class);
-			if (class_exists($controller_class)) {
-				$controller_class_list[$controller_class] = $app_base_namespace;
-			}
-		}
-		return $controller_class_list;
-	}
+  private static function scanControllerClasses(string $app_base_namespace, string $app_base_path): array
+  {
+    $controller_class_list = [];
+    $app_base_path = realpath($app_base_path) ?: $app_base_path;
+    $controller_files = static::scanControllerFiles($app_base_path);
+    foreach ($controller_files as $controller_file) {
+      $controller_class = preg_replace("/\.php$/i", '', $controller_file);
+      $controller_class = str_replace($app_base_path, $app_base_namespace, $controller_class);
+      $controller_class = str_replace("/", '\\', $controller_class);
+      if (class_exists($controller_class)) {
+        $controller_class_list[$controller_class] = $app_base_namespace;
+      }
+    }
+    return $controller_class_list;
+  }
 
-	private static function scanControllerFiles(string $controller_dir_path): array
-	{
-		$controller_files = [];
-		if (is_dir($controller_dir_path)) {
-			$dir_items = scandir($controller_dir_path);
-			foreach ($dir_items as $item) {
-				$item_realpath = $controller_dir_path . DIRECTORY_SEPARATOR . $item;
-				if (!preg_match("/^\..*/", $item) && is_dir($item_realpath)) {
-					array_push($controller_files, ...static::scanControllerFiles($item_realpath));
-				} elseif (preg_match("/[\/\\\]controller/i", $item_realpath) && preg_match("/\.php$/i", $item)) {
-					$controller_files[] = $item_realpath;
-				}
-			}
-		}
-		return $controller_files;
-	}
+  private static function scanControllerFiles(string $controller_dir_path): array
+  {
+    $controller_files = [];
+    if (is_dir($controller_dir_path)) {
+      $dir_items = scandir($controller_dir_path);
+      foreach ($dir_items as $item) {
+        $item_realpath = $controller_dir_path . DIRECTORY_SEPARATOR . $item;
+        if (!preg_match("/^\..*/", $item) && is_dir($item_realpath)) {
+          array_push($controller_files, ...static::scanControllerFiles($item_realpath));
+        } elseif (preg_match("/[\/\\\]controller/i", $item_realpath) && preg_match("/\.php$/i", $item)) {
+          $controller_files[] = $item_realpath;
+        }
+      }
+    }
+    return $controller_files;
+  }
 
-	/**
-	 * @throws ReflectionException
-	 */
-	private static function scanController(string $controller_class, string $app_base_namespace): void
-	{
-		$prefix = '';
-		/** 给定类的反射类 */
-		$controller_class_ref = new ReflectionClass($controller_class);
-		if (!!($base_path = $controller_class_ref->getAttributes(BasePath::class))) {
-			$prefix = $base_path[0]->newInstance()->path;
-		}
-		/** Controller 组中间件 */
-		$controller_middleware = $controller_class_ref->getAttributes(Middleware::class);
-		$controller_middleware = count($controller_middleware) > 0 ? $controller_middleware[0]->newInstance()->middleware : [];
-		/** 获取类里的所有方法 */
-		$controller_endpoints = $controller_class_ref->getMethods();
-		/** 遍历类方法 */
-		foreach ($controller_endpoints as $controller_endpoint) {
-			$endpoints = $controller_endpoint->getAttributes(RouteAttribute::class);
-			foreach ($endpoints as $endpoint) {
-				/**
-				 * 路由对象
-				 *
-				 * @var RouteAttribute $endpoint_route
-				 */
-				$endpoint_route = $endpoint->newInstance();
-				$endpoint_route->controllerClassRef = $controller_class_ref;
-				/** 设置的路由对象的参数列表 */
-				$arguments = $endpoint->getArguments();
-				// 处理 ./ 相对路径开头
-				$path = preg_replace("/^\.\//", '', $arguments[0] ?? $arguments['route'] ?? '');
-				if ($path === '') {
-					$path = $controller_endpoint->name === 'index' ? '' : $controller_endpoint->name;
-				}
-				// 路由对应方法，用于添加路由
-				$endpoint_method = $controller_class_ref->name . '@' . $controller_endpoint->name;
-				/** 相对路径子路由处理 */
-				if (!preg_match("/^[\/\\\]/", $path)) {
-					// 驼峰转换
-					$base_path = strtolower(preg_replace("/([a-z0-9])([A-Z])/", "$1-$2", $controller_class_ref->name));
-					$base_namespace = strtolower(preg_replace("/([a-z0-9])([A-Z])/", "$1-$2", $app_base_namespace));
-					// 反斜杠处理
-					$base_path = str_replace('\\', '/', $base_path);
-					$base_namespace = str_replace('\\', '/', $base_namespace);
-					$base_namespace = preg_replace('/^\//', '', $base_namespace); // 去除用户可能携带的开头斜杠
-					// 去除基本命名空间开头
-					$base_path = str_replace($base_namespace, '', $base_path);
-					// 路径中移除 controller 目录
-					$base_path = str_replace('/controller/', '/', $base_path);
-					// 拼接实际路径
-					$path = $base_path . (empty($path) ? '' : '/' . $path);
-				}
-				// 驼峰转换
-				$path = strtolower(preg_replace("/([a-z0-9])([A-Z])/", "$1-$2", $path));
-				// 移除 controller_suffix
-				$controller_suffix = strtolower(config('app.controller_suffix', 'controller'));
-				$path = preg_replace("/-$controller_suffix/", '', $path);
+  /**
+   * @throws ReflectionException
+   */
+  private static function scanController(string $controller_class, string $app_base_namespace): void
+  {
+    /** 给定类的反射类 */
+    $controller_class_ref = new ReflectionClass($controller_class);
 
-				$middleware_set_list = $endpoint_route->getMiddleware($controller_middleware);
-				$middleware_add_list = [];
-				foreach ($middleware_set_list as $middleware) {
-					if (
-						$middleware instanceof MiddlewareInterface
-						or (is_string($middleware) and class_exists($middleware))
-					) {
-						$middleware_add_list[] = $middleware;
-					}
-				}
-				/** 添加路由 */
-				$endpoint_route
-					->addToRoute($prefix . $path, $endpoint_method)
-					->name($controller_class_ref->getShortName())
-					->middleware($middleware_add_list);
-				if (
-					!$controller_class_ref->getAttributes(RemoveFromDoc::class)
-					&& !$controller_endpoint->getAttributes(RemoveFromDoc::class)
-				) {
-					/** 添加到 OpenAPI 文档 */
-					$endpoint_route->addToDoc($controller_endpoint);
-				}
-			}
-		}
-	}
+    $prefix = '';
+    if (!!($_prefix = $controller_class_ref->getAttributes(Prefix::class))) {
+      $prefix = $_prefix[0]->newInstance()->path;
+    }
+    $base_path = '';
+    if (!!($_basepath = $controller_class_ref->getAttributes(BasePath::class))) {
+      $base_path = $_basepath[0]->newInstance()->path;
+    }
 
-	/**
-	 * @throws ReflectionException
-	 */
-	private static function scanAppController($class_in_root): void
-	{
-		$class_ref = new ReflectionClass($class_in_root);
+    /** Controller 组中间件 */
+    $controller_middleware = $controller_class_ref->getAttributes(Middleware::class);
+    $controller_middleware = count($controller_middleware) > 0 ? $controller_middleware[0]->newInstance()->middleware : [];
 
-		$app_base_namespace = $class_ref->getNamespaceName();
-		$app_base_path = dirname($class_ref->getFileName());
+    /** 获取类里的所有方法 */
+    $controller_endpoints = $controller_class_ref->getMethods();
 
-		$controller_class_list = self::scanControllerClasses($app_base_namespace, $app_base_path);
+    /** 遍历类方法 */
+    foreach ($controller_endpoints as $controller_endpoint) {
+      $endpoints = $controller_endpoint->getAttributes(RouteAttribute::class);
+      foreach ($endpoints as $endpoint) {
+        /**
+         * 路由对象
+         *
+         * @var RouteAttribute $endpoint_route
+         */
+        $endpoint_route = $endpoint->newInstance();
+        $endpoint_route->controllerClassRef = $controller_class_ref;
+        /** 设置的路由对象的参数列表 */
+        $arguments = $endpoint->getArguments();
+        // 处理 ./ 相对路径开头
+        $path = preg_replace("/^\.\//", '', $arguments[0] ?? $arguments['route'] ?? '');
+        if ($path === '') {
+          $path = $controller_endpoint->name === 'index' ? '' : $controller_endpoint->name;
+        }
+        // 路由对应方法，用于添加路由
+        $endpoint_method = $controller_class_ref->name . '@' . $controller_endpoint->name;
+        /** 相对路径子路由处理 */
+        if (!preg_match("/^[\/\\\]/", $path)) {
+          if (!$base_path) {
+            // 驼峰转换
+            $base_path = strtolower(preg_replace("/([a-z0-9])([A-Z])/", "$1-$2", $controller_class_ref->name));
+            $base_namespace = strtolower(preg_replace("/([a-z0-9])([A-Z])/", "$1-$2", $app_base_namespace));
+            // 反斜杠处理
+            $base_path = str_replace('\\', '/', $base_path);
+            $base_namespace = str_replace('\\', '/', $base_namespace);
+            $base_namespace = preg_replace('/^\//', '', $base_namespace); // 去除用户可能携带的开头斜杠
+            // 去除基本命名空间开头
+            $base_path = str_replace($base_namespace, '', $base_path);
+            // 路径中移除 controller 目录
+            $base_path = str_replace('/controller/', '/', $base_path);
+          }
+          // 拼接实际路径
+          $path = $base_path . (empty($path) ? '' : '/' . $path);
+        }
+        // 驼峰转换
+        $path = strtolower(preg_replace("/([a-z0-9])([A-Z])/", "$1-$2", $path));
+        // 移除 controller_suffix
+        $controller_suffix = strtolower(config('app.controller_suffix', 'controller'));
+        $path = preg_replace("/-$controller_suffix/", '', $path);
 
-		foreach ($controller_class_list as $controller_class => $app_base_namespace) {
-			static::scanController($controller_class, $app_base_namespace);
-		}
-	}
+        $middleware_set_list = $endpoint_route->getMiddleware($controller_middleware);
+        $middleware_add_list = [];
+        foreach ($middleware_set_list as $middleware) {
+          if (
+            $middleware instanceof MiddlewareInterface
+            or (is_string($middleware) and class_exists($middleware))
+          ) {
+            $middleware_add_list[] = $middleware;
+          }
+        }
+        /** 添加路由 */
+        $endpoint_route
+          ->addToRoute($prefix . $path, $endpoint_method)
+          ->name($controller_class_ref->getShortName())
+          ->middleware($middleware_add_list);
+        if (
+          !$controller_class_ref->getAttributes(RemoveFromDoc::class)
+          && !$controller_endpoint->getAttributes(RemoveFromDoc::class)
+        ) {
+          /** 添加到 OpenAPI 文档 */
+          $endpoint_route->addToDoc($controller_endpoint);
+        }
+      }
+    }
+  }
 
-	private static function checkMenu(): void
-	{
-		$lock_file = fopen(runtime_path('wegar-menu.lock'), 'a+');
-		if (flock($lock_file, LOCK_EX)) {
-			try {
-				if (!class_exists(Menu::class)) {
+  /**
+   * @throws ReflectionException
+   */
+  private static function scanAppController($class_in_root): void
+  {
+    $class_ref = new ReflectionClass($class_in_root);
+
+    $app_base_namespace = $class_ref->getNamespaceName();
+    $app_base_path = dirname($class_ref->getFileName());
+
+    $controller_class_list = self::scanControllerClasses($app_base_namespace, $app_base_path);
+
+    foreach ($controller_class_list as $controller_class => $app_base_namespace) {
+      static::scanController($controller_class, $app_base_namespace);
+    }
+  }
+
+  private static function checkMenu(): void
+  {
+    $lock_file = fopen(runtime_path('wegar-menu.lock'), 'a+');
+    if (flock($lock_file, LOCK_EX)) {
+      try {
+        if (!class_exists(Menu::class)) {
 //					$host = config('server.listen');
 //					print "🚨 未安装 webman/admin 无法创建管理菜单，请自行访问文档: $host/wegar/swagger\n";
-				} else {
-					$dev_menu = Menu::get('dev');
-					if (!Menu::get(WegarController::class) && $dev_menu) {
-						$pid = $dev_menu['id'];
-						Menu::add([
-							'title' => 'Wegar Doc',
-							'href' => '/wegar/swagger',
-							'pid' => $pid,
-							'key' => WegarController::class,
-							'weight' => 0,
-							'type' => 1,
-						]);
-						print "✅ 创建 Wegar 管理菜单\n";
-					}
-				}
-			} catch (\Exception $exception) {
-				print "❌ 创建 Wegar 管理菜单\n";
-				print $exception->getMessage() . PHP_EOL;
-				print $exception->getTraceAsString() . PHP_EOL;
-			}
-		}
-	}
+        } else {
+          $dev_menu = Menu::get('dev');
+          if (!Menu::get(WegarController::class) && $dev_menu) {
+            $pid = $dev_menu['id'];
+            Menu::add([
+              'title' => 'Wegar Doc',
+              'href' => '/wegar/swagger',
+              'pid' => $pid,
+              'key' => WegarController::class,
+              'weight' => 0,
+              'type' => 1,
+            ]);
+            print "✅ 创建 Wegar 管理菜单\n";
+          }
+        }
+      } catch (\Exception $exception) {
+        print "❌ 创建 Wegar 管理菜单\n";
+        print $exception->getMessage() . PHP_EOL;
+        print $exception->getTraceAsString() . PHP_EOL;
+      }
+    }
+  }
 
-	public static function config($key, $default = null)
-	{
-		return config("plugin.qnnp.wegar.wegar.$key", $default);
-	}
+  public static function config($key, $default = null)
+  {
+    return config("plugin.qnnp.wegar.wegar.$key", $default);
+  }
 }
